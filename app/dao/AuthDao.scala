20f110fd62cc49ed587ec7ce8599c2db45f6e1ac
李凯利 <Kaili Li>
2016-06-08 19:08:41 +0800

    refactor auth service

--- app/dao/AuthDao.scala
+++ app/dao/AuthDao.scala
@@ -24,6 +24,27 @@ class AuthDao @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)
   import driver.api._
   //import slick.driver.MySQLDriver.api._
 
+  //NOTE: auth_type and third_party_id == "", is an anonymous user
+  def uuid_exists(client_id: String) = UserTable.filter(u => u.client_id === client_id && u.auth_type === "" && u.third_party_id === "" ).map(_.id).result.headOption
+  def uuid_create(client_id: String) = (UserTable.map(u => (u.client_id)) returning UserTable.map(_.id)) += (client_id)
+
+  def social_exists(client_id: String, auth_type: String, third_party_id: String) = UserTable.filter(u => u.auth_type === auth_type && u.third_party_id === third_party_id).map(_.id).result.headOption
+  def social_update_client_id(client_id: String, auth_type: String, third_party_id: String, u: Long) = {
+    for {
+      _ <- UserTable.filter(u => u.auth_type === auth_type && u.third_party_id === third_party_id).map(_.client_id).update(client_id)
+      i <- DBIO.successful(u)
+    } yield i
+  }
+
+  def social_update_social_info(client_id: String, auth_type: String, third_party_id: String, iu: Long) = {
+    for {
+      _ <- UserTable.filter(u => u.client_id === client_id && u.auth_type === "" && u.third_party_id === "" ).map(u => (u.auth_type, u.third_party_id)).update((auth_type, third_party_id))
+      x <- DBIO.successful(iu)
+    } yield x
+  }
+
+  def social_create(client_id: String, auth_type: String, third_party_id: String, name: String, avatar: String) = (UserTable.map(u => (u.client_id, u.auth_type, u.third_party_id, u.name, u.avatar)) returning UserTable.map(_.id)) += (client_id, auth_type, third_party_id, name, avatar)
+
   /**
     *
     * @param client_id
@@ -31,11 +52,10 @@ class AuthDao @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)
     */
   def uuid_login(client_id: String): Future[Long] = {
     val query = (for {
-      //NOTE: auth_type and third_party_id == "", is an anonymous user
-      idOpt <- UserTable.filter(u => u.client_id === client_id && u.auth_type === "" && u.third_party_id === "" ).map(_.id).result.headOption
+      idOpt <- uuid_exists(client_id)
       id <- idOpt match {
         case Some(i) => DBIO.successful(i)
-        case None => (UserTable.map(u => (u.client_id)) returning UserTable.map(_.id)) += (client_id)
+        case None => uuid_create(client_id)
       }
     } yield id).transactionally
 
@@ -53,23 +73,17 @@ class AuthDao @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)
     */
   def auth_login(client_id: String, auth_type: String, third_party_id: String, name: String, avatar: String): Future[Long] = {
     val query = (for {
-      idOpt <- UserTable.filter(u => u.auth_type === auth_type && u.third_party_id === third_party_id).map(_.id).result.headOption
+      idOpt <- social_exists(client_id, auth_type, third_party_id)
       id <- idOpt match {
           // is a Qidian User, update client_id
-        case Some(u) => for {
-          _ <- UserTable.filter(u => u.auth_type === auth_type && u.third_party_id === third_party_id).map(_.client_id).update(client_id)
-          i <- DBIO.successful(u)
-        } yield i
+        case Some(u) => social_update_client_id(client_id, auth_type, third_party_id, u)
           // a new Qidian User, try to merge with an anonymous user
         case None => for {
           // if client_id already exists,
-          idOpt <- UserTable.filter(u => u.client_id === client_id && u.auth_type === "" && u.third_party_id === "" ).map(_.id).result.headOption
+          idOpt <- uuid_exists(client_id)
           idu: Long <- idOpt match {
-            case Some(iu) => for {
-              _ <- UserTable.filter(u => u.client_id === client_id && u.auth_type === "" && u.third_party_id === "" ).map(u => (u.auth_type, u.third_party_id)).update((auth_type, third_party_id))
-              x <- DBIO.successful(iu)
-            } yield x
-            case None => (UserTable.map(u => (u.client_id, u.auth_type, u.third_party_id, u.name, u.avatar)) returning UserTable.map(_.id)) += (client_id, auth_type, third_party_id, name, avatar)
+            case Some(iu) => social_update_social_info(client_id, auth_type, third_party_id, iu)
+            case None => social_create(client_id, auth_type, third_party_id, name, avatar)
           }
         } yield idu
       }
