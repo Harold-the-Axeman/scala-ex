3b50ed838fcca38a496dbf7d6c4cc812c9e755ae
李凯利 <Kaili Li>
2016-07-05 19:46:57 +0800

    remove build.sbt; add ApplicationBuild.scala

---
+++ project/ApplicationBuild.scala
@@ -0,0 +1,66 @@
+import sbt._
+import sbt.Keys._
+import play.sbt.PlayImport._
+import play.sbt.PlayScala
+
+object ApplicationBuild extends Build {
+  //val branch = "git rev-parse --abbrev-ref HEAD".!!.trim
+  //val commit = "git rev-parse --short HEAD".!!.trim
+  val buildTime = (new java.text.SimpleDateFormat("yyyyMMdd-HHmmss")).format(new java.util.Date())
+  //val appVersion = "%s-%s-%s".format(branch, commit, buildTime)
+  val appVersion = "1.0"
+
+  val appScalaVersion = "2.11.7"
+
+  val commonDependencies = Seq(
+    jdbc,
+    cache,
+    ws,
+    specs2 % Test
+  )
+
+  val slickDependencies = Seq(
+    "com.typesafe.play" %% "play-slick" % "2.0.0",
+    "mysql" % "mysql-connector-java" % "5.1.39"
+  )
+
+  val dbLoggerDependencies = Seq(
+    "com.mchange" % "c3p0" % "0.9.2.1"
+  )
+
+  val swagggerApiDependencies = Seq (
+    "com.iheart" %% "play-swagger" % "0.3.3-PLAY2.5",
+    "org.webjars" % "swagger-ui" % "2.1.4"
+  )
+
+  val appResovlers = Seq (
+    "jcenterRepo" at "http://jcenter.bintray.com/"
+  )
+
+  val serviceADependencies = Seq() // You can have service specific dependencies
+  val serviceBDependencies = Seq()
+
+/*  val scalaBuildOptions = Seq("-unchecked", "-deprecation", "-feature", "-language:reflectiveCalls",
+    "-language:implicitConversions", "-language:postfixOps", "-language:dynamics","-language:higherKinds",
+    "-language:existentials", "-language:experimental.macros", "-Xmax-classfile-name", "140")*/
+  val scalaBuildOptions = Seq()
+
+  val dbProject = Project("qidian-db", file("services/qidian-db")).settings(
+    version := appVersion,
+    scalaVersion := appScalaVersion,
+    libraryDependencies ++= (commonDependencies ++ slickDependencies)
+  )
+
+  val qidianProject = Project("qidian", file(".")).enablePlugins(PlayScala).settings(
+    version := appVersion,
+    scalaVersion := appScalaVersion,
+    libraryDependencies ++= (commonDependencies ++ slickDependencies ++ dbLoggerDependencies ++ swagggerApiDependencies),
+    // This project runs both services together, which is mostly useful in development mode.
+    scalacOptions ++= scalaBuildOptions,
+    sources in doc in Compile := List(),
+    publishArtifact in packageDoc in Compile := false,
+    unmanagedResourceDirectories in Test <+= baseDirectory ( _ /"target/web/public/test" )
+  )
+  //.dependsOn(common % "test->test;compile->compile", serviceA % "test->test;compile->compile", serviceB % "test->test;compile->compile").aggregate(common, serviceA, serviceB)
+
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,4 +2,6 @@ logLevel := Level.Warn
 
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
+resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
+
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.3")
