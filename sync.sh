2a3daf3399ca8c2f4900d4a5069cd21851ccd2ef
李凯利 <Kaili Li>
2016-07-14 18:51:05 +0800

    fix sync.sh:2

--- sync.sh
+++ sync.sh
@@ -5,10 +5,11 @@ rsync -azv --delete conf/ prod1:~/conf
 rsync -azv --delete conf/ prod2:~/conf
 
 # App
-rsync -azv --delete services/app/target/universal/qidian-app-1.0.zip prod1:~/qidian-app-1.0.zip
-rsync -azv --delete services/app/target/universal/qidian-app-1.0.zip prod2:~/qidian-app-1.0.zip
+rsync -azv  services/app/target/universal/qidian-app-1.0.zip ~/qidian-app-1.0.zip
+rsync -azv  services/app/target/universal/qidian-app-1.0.zip prod1:~/qidian-app-1.0.zip
+rsync -azv  services/app/target/universal/qidian-app-1.0.zip prod2:~/qidian-app-1.0.zip
 # CMS
-rsync -azv --delete services/cms/target/universal/qidian-cms-1.0.zip ~/qidian-cms-1.0.zip
+rsync -azv  services/cms/target/universal/qidian-cms-1.0.zip ~/qidian-cms-1.0.zip
 # WS
-rsync -azv --delete services/ws/target/universal/qidian-ws-1.0.zip ~/qidian-ws-1.0.zip
+rsync -azv  services/ws/target/universal/qidian-ws-1.0.zip ~/qidian-ws-1.0.zip
 
